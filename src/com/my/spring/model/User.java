a7c4b3a8e656d3d216e594a0f0cf425fc8e381c3
PanruifengWawa <46937922@qq.com>
2016-10-22 16:45:09 +0800

    clear

---
+++ src/com/my/spring/model/User.java
@@ -0,0 +1,77 @@
+package com.my.spring.model;
+
+import java.sql.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+	private Long id;
+    private String userName;
+    private String password;
+    private String realName;
+    private Integer userType;
+    private String email;
+    private String tel;
+    private Date registerDate;
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Id
+    @Column(name = "user_name")
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRealName() {
+		return realName;
+	}
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	public Integer getUserType() {
+		return userType;
+	}
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+    
+    
+}
