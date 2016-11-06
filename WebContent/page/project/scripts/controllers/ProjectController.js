0a174996890bd187d5c5311527e352d545437002
xyxTest <1055337148@qq.com>
2016-11-06 14:57:21 +0800

    user page

--- WebContent/page/project/scripts/controllers/ProjectController.js
+++ WebContent/page/project/scripts/controllers/ProjectController.js
@@ -1,7 +1,7 @@
 function ProjectController($scope,ProjectService) {
 	console.log("载入ProjectController");
 	$scope.currentPage = 1;
-	$scope.userTofind = {};
+	$scope.ProjectTofind = {};
 	$scope.projectTitles=["序号","项目名称","项目编码","施工单位","项目负责人","设计单位","施工地点","项目简介","建设单位","版本","施工时间","施工周期","操作"];
 	//////显示增加项目界面
 	$scope.showProjectAdd = function(){
@@ -13,11 +13,11 @@ function ProjectController($scope,ProjectService) {
 	    ProjectService.findProject(projectId,token).then(function(result){
 	      $scope.findProjectInfo=result.data;
 	      document.getElementById("addProjectHtml").style.display = 'block';
-	      $scope.title="更新用户";
+	      $scope.title="更新项目";
 	    });
 	 }
 	  
-	  ///////分页获取用户列表
+	  ///////分页获取项目列表
 	$scope.getProjectList = function(pageSize,pageIndex,project) {
 		  ProjectService.getProjectList(pageSize,pageIndex,project).then(function (result){
 		  	  $scope.projectList = result.data;
@@ -44,71 +44,71 @@ function ProjectController($scope,ProjectService) {
 
 		  });
 	  }
-	  //////显示用户添加界面
-	  $scope.showaddUser = function() {
-		  $scope.findUserInfo = {};
-		  document.getElementById("addUserHtml").style.display = 'block';
-	      $scope.title="增加用户";
+	  //////显示项目添加界面
+	  $scope.showaddProject = function() {
+		  $scope.findProjectInfo = {};
+		  document.getElementById("addProjectHtml").style.display = 'block';
+	      $scope.title="增加项目";
 	  }
 	  
-	  ////显示更新用户界面
-	 $scope.userChangeClick = function(userId){
-	    UserService.findUser(userId,token).then(function(result){
-	      $scope.findUserInfo=result.data;
-	      document.getElementById("addUserHtml").style.display = 'block';
-	      $scope.title="更新用户";
+	  ////显示更新项目界面
+	 $scope.ProjectChangeClick = function(ProjectId){
+	    ProjectService.findProject(ProjectId,token).then(function(result){
+	      $scope.findProjectInfo=result.data;
+	      document.getElementById("addProjectHtml").style.display = 'block';
+	      $scope.title="更新项目";
 	    });
 	 }
 	 
-	 ///////更新用户
-	  $scope.updateUser = function(user){
-	  	UserService.updateUser(user,token).then(function(result){
-	       $scope.updateUserInfo=result.data;
+	 ///////更新项目
+	  $scope.updateProject = function(Project){
+	  	ProjectService.updateProject(Project,token).then(function(result){
+	       $scope.updateProjectInfo=result.data;
 	    });
 	  }
 	  
-	 /////删除用户
-	 $scope.deleteUser = function(userid){
-		  	UserService.deleteUser(userid,token).then(function(result){
-		       $scope.deleteUserInfo=result.data;
-		       $scope.getUserList();
+	 /////删除项目
+	 $scope.deleteProject = function(Projectid){
+		  	ProjectService.deleteProject(Projectid,token).then(function(result){
+		       $scope.deleteProjectInfo=result.data;
+		       $scope.getProjectList();
 		       
 		    });
 	 }
-	 /////返回用户列表
-	 $scope.returnUserlist = function(){
-		       $scope.getUserList();
-		       document.getElementById("addUserHtml").style.display = 'none';
+	 /////返回项目列表
+	 $scope.returnProjectlist = function(){
+		       $scope.getProjectList();
+		       document.getElementById("addProjectHtml").style.display = 'none';
 	 }
-	 //////重置添加用户信息
-	 $scope.resetUser = function(){
-		 $scope.findUserInfo = {};
+	 //////重置添加项目信息
+	 $scope.resetProject = function(){
+		 $scope.findProjectInfo = {};
 	 }
 
-	 /////增加用户
-	 $scope.addUserByAdmin = function(){
-		 if($scope.title=="增加用户"){
-			 findUserInfo=$scope.findUserInfo;
-			 UserService.addUserByAdmin(findUserInfo,token).then(function(result){
-			       $scope.addUserByAdminInfo=result.data;
-			       $scope.getUserList(pageSize,1,$scope.userTofind);
+	 /////增加项目
+	 $scope.addProjectByAdmin = function(){
+		 if($scope.title=="增加项目"){
+			 findProjectInfo=$scope.findProjectInfo;
+			 ProjectService.addProjectByAdmin(findProjectInfo,token).then(function(result){
+			       $scope.addProjectByAdminInfo=result.data;
+			       $scope.getProjectList(pageSize,1,$scope.ProjectTofind);
 			       
 			    });
 		 }
-		 if($scope.title=="更新用户")
+		 if($scope.title=="更新项目")
 		{
-			 UserService.updateUser($scope.findUserInfo,token).then(function(result){
-			       $scope.updateUserInfo=result.data;
-			       $scope.getUserList(pageSize,1,$scope.userTofind);
+			 ProjectService.updateProject($scope.findProjectInfo,token).then(function(result){
+			       $scope.updateProjectInfo=result.data;
+			       $scope.getProjectList(pageSize,1,$scope.ProjectTofind);
 			       
 			    });
 		}
 		 
 	 }
-	 ////////模糊查找用户
+	 ////////模糊查找项目
 	 $scope.find = function() {
 		 $scope.currentPage = 1;
-		 $scope.getUserList(pageSize,$scope.currentPage,$scope.userTofind);
+		 $scope.getProjectList(pageSize,$scope.currentPage,$scope.ProjectTofind);
 	 }
-	 $scope.getUserList(pageSize,$scope.currentPage,$scope.userTofind);
+	 $scope.getProjectList(pageSize,$scope.currentPage,$scope.ProjectTofind);
 }
--- WebContent/page/project/scripts/services/ProjectService.js
+++ WebContent/page/project/scripts/services/ProjectService.js
@@ -1,117 +1,167 @@
-angular.module('Demo')
-  .service('ProjectService', function ProjectService($http, $q) {
-
+  var token=getCookie('token');
+  angular.module('Demo')
+.service('ProjectService', function ProjectService($http, $q) {
   
-    var self=this;
-   
-    //獲取项目列表
-    this.getProjectList = function(token) {
+  var transform = function(data){
+        return $.param(data);
+    }
+  var self=this;
 
-        var deferred = $q.defer();
-        console.log("读取ProjectList数据");
-        $http.get('api/project/getProjectList?token='+token)
-            .success(function(data, status, headers, config){
-                console.log(data);
-                if(data.callStatus == "SUCCEED"){
-                    deferred.resolve(data);
-                    self.ProjectList = data;
+  /////注册用户
+  this.register = function(Project) {
 
-                }else{
-                    alert("数据读取失败");
-                }
-                
-            })
-            .error(function(data, status, headers, config){
-                deferred.reject(data);
-            });
-        return deferred.promise;
-        };
-        //////删除项目信息
-     this.deleteProject = function(projectId,token,modelId) {
-        
-        var deferred = $q.defer();
-        console.log("删除Project数据");
-        $http.get('api/project/deleteProject?id='+projectId+'&token='+token+'&modelid='+modelId)
-            .success(function(data, status, headers, config){
-                console.log(data);
-                if(data.callStatus == "SUCCEED"){
-                    deferred.resolve(data);
-                    self.deleteProjectInfo = data;
+      var deferred = $q.defer();
+      console.log("读取ProjectList数据");
+      $http.post('api/Project/admin/getProjectList?token='+getCookie('token'))
+          .success(function(data, status, headers, config){
+              console.log(data);
+              if(data.callStatus == "SUCCEED"){
+                  deferred.resolve(data);
+                  self.ProjectList = data;
+             
+              }else{
+                  alert("数据读取失败");
+              }
+              
+          })
+          .error(function(data, status, headers, config){
+              deferred.reject(data);
+          });
+      return deferred.promise;
+      };
+  //獲取用戶列表
+  this.getProjectList = function(pageSize,pageIndex,Project) {
 
-                }else{
-                    alert("数据删除失败");
-                }
-                
-            })
-            .error(function(data, status, headers, config){
-                deferred.reject(data);
-            });
-        return deferred.promise;
-        };
-        ///////更新用户信息
-        this.updateProject = function(project,token) {
-        
-        var deferred = $q.defer();
-        console.log("更新Project数据");
-        $http.post('api/project/updateProject',project)
-            .success(function(data, status, headers, config){
-                console.log(data);
-                if(data.callStatus == "SUCCEED"){
-                    deferred.resolve(data);
-                    self.updateProjectInfo = data;
+      var deferred = $q.defer();
+      console.log("读取ProjectList数据");
+      var api = 'api/Project/admin/getProjectList?token='+getCookie('token') + "&pageSize=" + pageSize + "&pageIndex="+pageIndex;
+      if(Project.ProjectName !== undefined && trimStr(Project.ProjectName) != '') {
+      	api += "&ProjectName="+trimStr(Project.ProjectName);
+      }
+      if(Project.realName !== undefined && trimStr(Project.realName) != ''){
+      	api += "&realName="+trimStr(Project.realName);
+      }
+      if(Project.email !==undefined && trimStr(Project.email) != ''){
+      	api += "&email="+ trimStr(Project.email);
+      }
+      if(Project.tel !==undefined && trimStr(Project.tel) != ''){
+      	api += "&tel="+ trimStr(Project.tel);
+      }
+      $http.get(encodeURI(api))
+          .success(function(data, status, headers, config){
+              if(data.callStatus == "SUCCEED"){
+                  deferred.resolve(data);
+                  self.ProjectList = data;
+             
+              }else{
+                  alert("数据读取失败");
+              }
+              
+          })
+          .error(function(data, status, headers, config){
+              deferred.reject(data);
+          });
+      return deferred.promise;
+      };
+      //////删除用户信息
+   this.deleteProject = function(ProjectId) {
+      
+      var deferred = $q.defer();
+      console.log("删除Project数据");
+      $http.get('api/Project/admin/deleteProject?ProjectId='+ProjectId+'&token='+getCookie('token'))
+          .success(function(data, status, headers, config){
+              console.log(data);
+              if(data.callStatus == "SUCCEED"){
+                  deferred.resolve(data);
+                  self.deleteProjectInfo = data;
+                  alert("数据删除成功");
+              }else{
+                  alert("数据删除失败");
+              }
+              
+          })
+          .error(function(data, status, headers, config){
+              deferred.reject(data);
+          });
+      return deferred.promise;
+      };
+      ///////更新用户信息
+      this.updateProject = function(Project,token) {
+      	var deferred = $q.defer();
+      	console.log("更新Project数据");
+      	delete Project.registerDate;
+//	          	var nProject = {};
+//	          	nProject.id = Project.id;
+//	          	nProject.realName = Project.realName;
+      	$http.post('api/Project/admin/updateProject?token='+token,Project,
+      		{
+      			headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+      			transformRequest: transform
+      		})
+          .success(function(data, status, headers, config){
+          	console.log(data);
+              if(data.callStatus == "SUCCEED"){
+                  deferred.resolve(data);
+                  self.updateProjectInfo = data;
+                  alert("更新成功")
+              }else{
+                  alert("数据更新失败("+data.errorCode+")");
+              }
+              
+          })
+          .error(function(data, status, headers, config){
+              deferred.reject(data);
+          });
+      return deferred.promise;
+      };
+      ///////增加用户信息
+      this.addProjectByAdmin = function(findProjectInfo,token) {
+          var deferred = $q.defer();
+          
+          
+          console.log("查找Project数据");
+          $http.post('api/Project/admin/addProject?token='+token,findProjectInfo,
+          		{
+          			headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+          			transformRequest: transform
+          		})
+              .success(function(data, status, headers, config){
 
-                }else{
-                    alert("数据更新失败");
-                }
-                
-            })
-            .error(function(data, status, headers, config){
-                deferred.reject(data);
-            });
-        return deferred.promise;
-        };
-        ///////根据Id查找用户信息
-        this.findProject = function(projectId,token) {
-        
-        var deferred = $q.defer();
-        console.log("查找Project数据");
-        $http.get('api/project/getProjectDetails?projectId='+projectId+'&token='+token)
-            .success(function(data, status, headers, config){
-                console.log(data);
-                if(data.callStatus == "SUCCEED"){
-                    deferred.resolve(data);
-                    self.findUserInfo = data;
-
-                }else{
-                    alert("数据查找失败");
-                }
-                
-            })
-            .error(function(data, status, headers, config){
-                deferred.reject(data);
-            });
-        return deferred.promise;
-        };
- /* ///////模糊查找用户信息
-        this.findProjectByLike = function(project,token) {
-        
-        var deferred = $q.defer();
-        console.log("查找Project数据");
-        $http.get('api/project/findProjectLike?=name'+project.name+'&num='+project.num)
-            .success(function(data, status, headers, config){
-                console.log(data);
-                if(data.callStatus == "SUCCEED"){
-                    deferred.resolve(data);
-                    self.findUserInfo = data;
-
-                }else{
-                    alert("数据查找失败");
-                }
-                
-            })
-            .error(function(data, status, headers, config){
-                deferred.reject(data);
-            });
-        return deferred.promise;
-        };*/
-});
+                  if(data.callStatus == "SUCCEED"){
+                      deferred.resolve(data);
+                      alert("添加成功")
+                      self.findProjectInfo = data;
+                  
+                  }else{
+                      alert("数据添加失败("+data.errorCode+")");
+                  }
+                  
+              })
+              .error(function(data, status, headers, config){
+                  deferred.reject(data);
+              });
+          return deferred.promise;
+          };
+      ///////查找用户信息
+      this.findProject = function(ProjectId,token) {
+      
+      var deferred = $q.defer();
+      console.log("查找Project数据");
+      $http.get('api/Project/admin/getProjectDetails?ProjectId='+ProjectId+'&token='+token)
+          .success(function(data, status, headers, config){
+              console.log(data);
+              if(data.callStatus == "SUCCEED"){
+                  deferred.resolve(data);
+                  self.findProjectInfo = data;
+              
+              }else{
+                  alert("数据查找失败");
+                  }
+                  
+              })
+              .error(function(data, status, headers, config){
+                  deferred.reject(data);
+              });
+          return deferred.promise;
+          };
+  });
